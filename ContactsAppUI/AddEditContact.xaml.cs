86e9a63d69bcddc060c8421513a9607f09c81d75
Alexandra Dyagay <dyagaay@gmail.com>
2021-02-26 21:09:47 +0700

    Добавлены окна

---
+++ ContactsAppUI/AddEditContact.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ContactsAppUI
+{
+    /// <summary>
+    /// Interaction logic for AddEditContact.xaml
+    /// </summary>
+    public partial class AddEditContact : Window
+    {
+        public AddEditContact()
+        {
+            InitializeComponent();
+        }
+    }
+}
