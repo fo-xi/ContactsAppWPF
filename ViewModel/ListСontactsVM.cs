7949a6029a05c72716e4b10c4a0a3620a6153e57
Alexandra Dyagay <dyagaay@gmail.com>
2021-03-01 17:29:43 +0700

    Привязка данных к listBox и панели с информацией о контакте

---
+++ ViewModel/ListСontactsVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ContactsApp;
+
+namespace ViewModel
+{
+    public class ListСontactsVM : INotifyPropertyChanged
+
+    {
+        private Contact _selectedContact;
+
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        public Contact SelectedContact
+        {
+            get { return _selectedContact; }
+            set
+            {
+                _selectedContact = value;
+                OnPropertyChanged("SelectedContact");
+            }
+        }
+
+        public ListСontactsVM()
+        {
+            Contacts = new ObservableCollection<Contact>
+            {
+                new Contact("Sasha", "Dyagay", 
+                    new PhoneNumber("79234657789"), 
+                    new DateTime(2020, 8, 23), 
+                    "jnglsnl@kmh.com", "895974"),
+                new Contact("Dasha", "Koko", 
+                    new PhoneNumber("79456735567"), 
+                    new DateTime(2000, 4, 11), 
+                    "nmhlrt@jmnl.com", "83963"),
+                new Contact("Masha", "Goktr", 
+                    new PhoneNumber("79237568844"), 
+                    new DateTime(2005, 3, 10), 
+                    "imjtm@jmnl.com", "486745")
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ViewModel/MainWindowVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class MainWindowVM
+    {
+        public ListСontactsVM ListСontacts { get; set; }
+
+        public MainWindowVM()
+        {
+            ListСontacts = new ListСontactsVM();
+        }
+    }
+}
