52787a014dcbfcab8fcbe6d3ba5605c54994d97e
Alexandra Dyagay <dyagaay@gmail.com>
2021-03-01 19:26:24 +0700

    Добавлена связка констрола Birthday и метода GetBirthday

---
+++ ViewModel/BirthdayVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactsApp;
+
+namespace ViewModel
+{
+    public class BirthdayVM
+    {
+        public string ListBirthdayContact { get; set; }
+
+        public BirthdayVM(List<Contact> contacts)
+        {
+            ListBirthdayContact = GetString(contacts);
+        }
+
+        private string GetString(List<Contact> contacts)
+        {
+            string stringContacts = string.Empty;
+
+            foreach (var i in contacts)
+            {
+                stringContacts += i.Surname + " ";
+            }
+
+            return stringContacts;
+        }
+
+    }
+}
--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -1,18 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContactsApp;
 
 namespace ViewModel
 {
     public class MainWindowVM
     {
+        public Project _project;
+
+        public BirthdayVM Birthday { get; set; }
+
         public ListСontactsVM ListСontacts { get; set; }
 
         public MainWindowVM()
         {
+            _project = ProjectManager.ReadFromFile();
+            _project.Contacts = new List<Contact>
+            {
+                new Contact("Sasha", "Dyagay",
+                    new PhoneNumber("79234657789"),
+                    DateTime.Now,
+                    "jnglsnl@kmh.com", "895974"),
+                new Contact("Dasha", "Koko",
+                    new PhoneNumber("79456735567"),
+                    DateTime.Now,
+                    "nmhlrt@jmnl.com", "83963"),
+                new Contact("Masha", "Goktr",
+                    new PhoneNumber("79237568844"),
+                    new DateTime(2005, 3, 10),
+                    "imjtm@jmnl.com", "486745")
+            };
             ListСontacts = new ListСontactsVM();
+            var listBirthContacts = _project.GetDateBirth(DateTime.Now);
+            Birthday = new BirthdayVM(listBirthContacts);
         }
     }
 }
